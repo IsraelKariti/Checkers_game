add30d425fc9257f94cc3fe0d7543889ec14f64a
izi2bhappy <izi2bhappy@gmail.com>
2022-03-14 00:40:56 +0200

    Set all pawns (blue and red) on board

--- Assets/CheckersManager.cs
+++ Assets/CheckersManager.cs
@@ -22,6 +22,23 @@ public class CheckersManager : MonoBehaviour
     private void InitBoard()
     {
         InitBlue();
+        InitRed();
+    }
+
+    private void InitRed()
+    {
+        // position of the red left bottom corner
+        float xPos = 0.15f;
+        float zPos = 0.55f;
+
+        // initialize the first line
+        InitLine(xPos, zPos, pawnRedPrefab);
+
+        // initialize the first line
+        InitLine(xPos - 0.1f, zPos + 0.1f, pawnRedPrefab);
+
+        // initialize the first line
+        InitLine(xPos, zPos + 0.2f, pawnRedPrefab);
     }
 
     private void InitBlue()
@@ -31,26 +48,23 @@ public class CheckersManager : MonoBehaviour
         float zPos = 0.05f;
 
         // initialize the first line
-        InitLine(xPos, zPos);
+        InitLine(xPos, zPos, pawnBluePrefab);
 
         // initialize the first line
-        InitLine(xPos+0.1f, zPos+0.1f);
+        InitLine(xPos+0.1f, zPos+0.1f, pawnBluePrefab);
 
         // initialize the first line
-        InitLine(xPos, zPos+0.2f);
-
-
-
+        InitLine(xPos, zPos+0.2f, pawnBluePrefab);
 
     }
 
-    private void InitLine(float xPos, float zPos)
+    private void InitLine(float xPos, float zPos, GameObject prefab)
     {
         GameObject temp;
 
         for (int i = 0; i < 4; i++)
         {
-            temp = Instantiate(pawnBluePrefab, board.transform);
+            temp = Instantiate(prefab, board.transform);
             temp.transform.localPosition = new Vector3(xPos, 0.07f, zPos);
             bluePawnList.Add(temp);
             xPos += 0.2f;
