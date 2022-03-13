b7d89deb850977f7150ce85f2dc659a7083b7b55
izi2bhappy <izi2bhappy@gmail.com>
2022-03-14 00:31:28 +0200

    set all blue pawns from script

--- Assets/CheckersManager.cs
+++ Assets/CheckersManager.cs
@@ -26,9 +26,35 @@ public class CheckersManager : MonoBehaviour
 
     private void InitBlue()
     {
-        GameObject temp = Instantiate(pawnBluePrefab, board.transform);
-        temp.transform.localPosition = new Vector3(0.05f, 0.07f, 0.05f);
-        bluePawnList.Add(temp);
+        // position of the bottom left corner
+        float xPos = 0.05f;
+        float zPos = 0.05f;
+
+        // initialize the first line
+        InitLine(xPos, zPos);
+
+        // initialize the first line
+        InitLine(xPos+0.1f, zPos+0.1f);
+
+        // initialize the first line
+        InitLine(xPos, zPos+0.2f);
+
+
+
+
+    }
+
+    private void InitLine(float xPos, float zPos)
+    {
+        GameObject temp;
+
+        for (int i = 0; i < 4; i++)
+        {
+            temp = Instantiate(pawnBluePrefab, board.transform);
+            temp.transform.localPosition = new Vector3(xPos, 0.07f, zPos);
+            bluePawnList.Add(temp);
+            xPos += 0.2f;
+        }
     }
 
     // Update is called once per frame
