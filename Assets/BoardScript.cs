c8cb40a7efb951aa26aec7dad24f7daa0557d116
izi2bhappy <izi2bhappy@gmail.com>
2022-03-15 13:29:02 +0200

    Make board moveable + make pawn kinematic

---
+++ Assets/BoardScript.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardScript : MonoBehaviour
+{
+    public void ResolveStartMovement()
+    {
+        
+    }
+    // This function is called when the user release the grip 
+    public void ResolveEndMovement()
+    {
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
