145b5742820d33f8d648c118c4abc17c3b0a6fe4
izi2bhappy <izi2bhappy@gmail.com>
2022-03-20 15:44:47 +0200

    FIXED BUG: Raycast failure prevented from Board becoming not kinematic after hand release

--- Assets/Scripts/BoardScript.cs
+++ Assets/Scripts/BoardScript.cs
@@ -8,21 +8,24 @@ public class BoardScript : MonoBehaviour
     
     public void ResolveStartMovement()
     {
-        
+        Debug.Log("STARTTT");
     }
     // This function is called when the user release the grip 
     public void ResolveEndMovement()
     {
+        Debug.Log("ENDDD");
 
         //REMEMBER: MakePlanes() is called from the OnManipulationEvent directly (attached in the inspector)
 
         //TODO: check if there is a table below me
         RaycastHit hit;
-        Physics.Raycast(transform.position, Vector3.down, out hit, 10.0f);
-        // if the object beneath the board is a table put the board on it
-        if(hit.transform.gameObject.GetComponent<SpatialAwarenessPlanarObject>().SurfaceType == SpatialAwarenessSurfaceTypes.Platform)
+        if(Physics.Raycast(transform.position, Vector3.down, out hit, 10.0f))
         {
-            transform.position = hit.point;
+            // if the object beneath the board is a table put the board on it
+            if( hit.transform.gameObject.GetComponent<SpatialAwarenessPlanarObject>().SurfaceType == SpatialAwarenessSurfaceTypes.Platform)
+            {
+                transform.position = hit.point;
+            }
         }
     }
 
